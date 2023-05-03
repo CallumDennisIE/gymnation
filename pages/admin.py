9d7f286762b970c1fcb001c83e030822b5ed7108
CallumDennisIE <callumdennisie@gmail.com>
2023-05-03 20:11:20 +0000

    feat: add Session model and admin panel integration

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Session
 
-# Register your models here.
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('location', 'created_on')
+    list_display = ('title', 'slug', 'trainer', 'location')
+    search_fields = ['title', 'trainer__username', 'location']
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Session(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    about = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    date = models.DateField()
+    trainer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="gym_session")
+    capacity = models.IntegerField(default=0)
+    attendance = models.ManyToManyField(
+        User, related_name='session_attendance', blank=True)
+    location = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_attending(self):
+        return self.attendance.count()
