a1ab09f3cebf6b2bc95752fe07a4b6bf5fc3edc7
CallumDennisIE <callumdennisie@gmail.com>
2023-05-07 21:03:08 +0000

    fix: wire up all urls to correct html pages

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.SessionList.as_view(), name='home'),
-    path('<slug:slug>', views.SessionDetail.as_view(), name='session_detail')
+    path('', views.get_about_page, name='about'),
+    path('<slug:slug>', views.SessionDetail.as_view(), name='session_detail'),
+    path('classes/', views.SessionList.as_view(), name='classes'),
+    path('our-team/', views.get_our_team_page, name='our_team'),
+    path('shop/', views.get_shop_page, name='shop'),
+    path('contact-us/', views.get_contact_us_page, name='contact_us')
 ]
